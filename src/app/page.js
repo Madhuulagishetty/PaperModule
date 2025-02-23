6a3338bafd285e304c9c22d6b7cf1dfa62e20859
Madhuulagishetty <lagishettymadhu05@gmail.com>
2025-02-24 02:08:39 +0530

    PaperModule Project Started

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+
+import Login from './Components/login/Login'
+import GradeSelector from './GradeSelector/page'
+export default function Home() {
+  return (
+    <>
+      <Login/>
+      <GradeSelector/>
+    </>
+    
+  );
+}
